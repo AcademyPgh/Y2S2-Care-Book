aa66e6f5d836e52d862d17ac6adf86d6fa3a2a10
wingroove <wing.groove@gmail.com>
2017-12-05 15:04:56 -0500

    resolved dr issues with add and remove buttons, typing into fields. Added remove physician button

--- src/Doctors.js
+++ src/Doctors.js
@@ -16,22 +16,30 @@ class DoctorForm extends Component{
         this.handleDeleteDoctor = this.handleDeleteDoctor.bind(this);
       }
 
-    handleAddPhone(drIndex) { //this adds a phone number box
-        var tempProfile = this.state.profile;
-        tempProfile.doctors[drIndex].phones.push({label: '', number: ''}) ;
-        this.setState({profile: tempProfile});
+    handleAddPhone(drIndex) { //this adds a phone number
+        return () =>{
+            var tempProfile = this.state.profile;
+            tempProfile.doctors[drIndex].phones.push({label: '', number: ''}) ;
+            this.setState({profile: tempProfile});
+        }
     }      
 
-    handleDeletePhone(phoneIndex,drIndex) { //this deletes a phone number box
-        var hi = this.state.profile;
+    handleDeletePhone(phoneIndex,drIndex) { //this deletes a phone number
+        var temp = this.state.profile;            
+        console.log(`dr index: ${JSON.stringify(drIndex)}`)     
+            console.log(`phone index: ${JSON.stringify(phoneIndex)}`)       
+            console.log(`doctors length: ${JSON.stringify(temp.doctors.length)}`)        
+            console.log(`doctors: ${JSON.stringify(temp.doctors[0])}`)
+            console.log(`doctors length: ${JSON.stringify(temp.doctors[0].phones[0])}`)  
         if (phoneIndex > 0)
         {
-        hi.doctors[drIndex].phones.splice(phoneIndex, 1);
-        this.setState({profile: hi});
+        temp.doctors[drIndex].phones.splice(phoneIndex, 1);
+        this.setState({profile: temp});
         }
     }
 
-    handleAddAddress(drIndex) { //this adds an address box
+    handleAddAddress(drIndex) { //this adds an address
+        return () =>{
         var tempProfile = this.state.profile;
         tempProfile.doctors[drIndex].addresses.push({label: "",
         line1: "", 
@@ -40,14 +48,15 @@ class DoctorForm extends Component{
         state: "", 
         zip: ""}) ;
         this.setState({profile: tempProfile});
+        }
     }      
 
-    handleDeleteAddress(addressIndex,drIndex) { //this deletes an address box
-        var hi = this.state.profile;
+    handleDeleteAddress(addressIndex,drIndex) { //this deletes an address
+        var temp = this.state.profile;
         if (addressIndex > 0)
         {
-        hi.doctors[drIndex].addresses.splice(addressIndex, 1);
-        this.setState({profile: hi});
+        temp.doctors[drIndex].addresses.splice(addressIndex, 1);
+        this.setState({profile: temp});
         }
     }
 
@@ -55,8 +64,8 @@ class DoctorForm extends Component{
         var tempProfile = this.state.profile;
         tempProfile.doctors.push({
             label: "",
-            firstName: "Andrew",
-            lastName: "Yeung",
+            firstName: "",
+            lastName: "",
             phones: [{
                 label: "",
                 number: ""
@@ -82,8 +91,9 @@ class DoctorForm extends Component{
         this.setState({profile: temp});
         }
     }
+
     render() {
-        const doctors = this.state.profile.doctors;
+        const doctors = this.state.profile.doctors;                        
         return (
           <div><br/>
                 <form id="form2">
@@ -91,26 +101,26 @@ class DoctorForm extends Component{
                     {doctors.map((doctor, drIndex) => {
                         var btnP = '';
                         if(drIndex>0){
-                        btnP = <button type="button" onClick={() => {this.handleDeleteDoctor(drIndex)}} className="small">Delete Doctor</button>;
+                        btnP = <button type="button" onClick={() => {this.handleDeleteDoctor(drIndex)}} className="small">Delete Physician</button>;
                         }
                         return (<div> 
-                            <h3>Physician {drIndex+1} </h3>
+                            <h3>Physician {drIndex+1} {btnP}</h3> 
                             <label htmlFor="firstName"></label>
-                                <input className = "text" field="firstName" id="firstName" value ={doctors[drIndex].firstName} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].firstName = e.target.value; this.setState({profile: tempProfile})}} placeholder = "First Name"/>
+                                <input className = "text" field="firstName" id="firstName" value ={doctor.firstName} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].firstName = e.target.value; this.setState({profile: tempProfile})}} placeholder = "First Name"/>
                             <label htmlFor="lastName"></label>
-                                <input className = "text" field="lastName" id="lastName" value={doctors[drIndex].lastName} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].lastName = e.target.value; this.setState({profile: tempProfile})}}  placeholder = "Last Name"/>
+                                <input className = "text" field="lastName" id="lastName" value={doctor.lastName} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].lastName = e.target.value; this.setState({profile: tempProfile})}}  placeholder = "Last Name"/>
                             <label>
-                                <select value={doctors[drIndex].label} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].label = e.target.value; this.setState({profile: tempProfile})}}>
+                                <select value={doctor.label} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].label = e.target.value; this.setState({profile: tempProfile})}}>
                                 <option value="pcp">Primary Care Physician</option>
                                 <option value="specialist">Specialist</option>
                                 </select>
                             </label><br/>
                             <label htmlFor="email"></label>
-                            <input className = "text" field="email" id="email" value={doctors[drIndex].email} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].email = e.target.value; this.setState({profile: tempProfile})}} placeholder="Email"/>
-                            {doctors[drIndex].phones.map((phone, phoneIndex) => {
+                            <input className = "text" field="email" id="email" value={doctor.email} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].email = e.target.value; this.setState({profile: tempProfile})}} placeholder="Email"/>
+                            {doctor.phones.map((phone, phoneIndex) => {
                                 var btn = '';
                                 if(phoneIndex>0){
-                                btn = <button type="button" onClick={() => {this.handleDeletePhone(phoneIndex)}} className="small">Delete</button>;
+                                btn = <button type="button" onClick={() => {this.handleDeletePhone(phoneIndex, drIndex)}} className="small">Delete</button>;
                                 }
                                 return (<div> 
                                     <h4>Physician Phone Number {phoneIndex+1}</h4>
@@ -119,28 +129,28 @@ class DoctorForm extends Component{
                                 {btn}<br/>
                                 </div>);
                             })}
-                                <button type="button" onClick={this.handleAddPhone} className="small">Add Phone Numbers</button><br/>
-                            {doctors[drIndex].addresses.map((address, addressIndex) => {
+                                <button type="button" onClick={this.handleAddPhone(drIndex)} className="small">Add Phone Numbers</button><br/>
+                            {doctor.addresses.map((address, addressIndex) => {
                                 var btnA = '';
                                 if(addressIndex>0){
-                                btnA = <button type="button" onClick={() => {this.handleDeleteAddress(addressIndex)}} className="small">Delete</button>;
+                                btnA = <button type="button" onClick={() => {this.handleDeleteAddress(addressIndex, drIndex)}} className="small">Delete</button>;
                                 }
                                 return (<div> 
                                     <h4>Physician Address {addressIndex+1}</h4>
                                     <label htmlFor="addressLine1"></label>
-                                        <input className = "text" field="line1" id="line1" value={doctors[drIndex].addresses[addressIndex].line1} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].addresses[addressIndex].line1 = e.target.value; this.setState({profile: tempProfile})}}  placeholder="Address Line 1"/>
+                                        <input className = "text" field="line1" id="line1" value={doctor.addresses[addressIndex].line1} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].addresses[addressIndex].line1 = e.target.value; this.setState({profile: tempProfile})}}  placeholder="Address Line 1"/>
                                     <label htmlFor="addressLine2" ></label>
-                                        <input className = "text" field="line2" id="line2" value={doctors[drIndex].addresses[addressIndex].line2} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].addresses[addressIndex].line2 = e.target.value; this.setState({profile: tempProfile})}} placeholder="Address Line 2"/><br/>
+                                        <input className = "text" field="line2" id="line2" value={doctor.addresses[addressIndex].line2} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].addresses[addressIndex].line2 = e.target.value; this.setState({profile: tempProfile})}} placeholder="Address Line 2"/><br/>
                                     <label htmlFor="addressCity" ></label>
-                                        <input className = "text" field="city" id="city" value={doctors[drIndex].addresses[addressIndex].city} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].addresses[addressIndex].city = e.target.value; this.setState({profile: tempProfile})}} placeholder="City"/>
+                                        <input className = "text" field="city" id="city" value={doctor.addresses[addressIndex].city} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].addresses[addressIndex].city = e.target.value; this.setState({profile: tempProfile})}} placeholder="City"/>
                                     <label htmlFor="addressState" ></label>
-                                        <input className = "text" field="state" id="state" value={doctors[drIndex].addresses[addressIndex].state} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].addresses[addressIndex].state = e.target.value; this.setState({profile: tempProfile})}} placeholder="State"/>
+                                        <input className = "text" field="state" id="state" value={doctor.addresses[addressIndex].state} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].addresses[addressIndex].state = e.target.value; this.setState({profile: tempProfile})}} placeholder="State"/>
                                     <label htmlFor="addressZip" ></label>
-                                        <input className = "text" field="zip" id="zip" value={doctors[drIndex].addresses[addressIndex].zip} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].addresses[addressIndex].zip = e.target.value; this.setState({profile: tempProfile})}} placeholder="Zip Code"/><br/>
+                                        <input className = "text" field="zip" id="zip" value={doctor.addresses[addressIndex].zip} onChange={e => {var tempProfile = this.state.profile; tempProfile.doctors[drIndex].addresses[addressIndex].zip = e.target.value; this.setState({profile: tempProfile})}} placeholder="Zip Code"/><br/>
                                         {btnA}<br/>
                                 </div>);
                             })}
-                            <button type="button" onClick={this.handleAddAddress} className="small">Add Addresses</button><br/>
+                            <button type="button" onClick={this.handleAddAddress(drIndex)} className="small">Add Addresses</button><br/>
                         </div>);
                     })}
                     <button type="button" onClick={this.handleAddDoctor} className="small">Add Another Physician</button><br/>
